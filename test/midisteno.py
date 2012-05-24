f6180d207509e0f6c25f437a02fc228cfe146232
Nate <nate@darkstar.example.net>
2012-05-24 15:59:38 -0700

    midisteno has flushing output and midi input

--- test/midisteno.py
+++ test/midisteno.py
@@ -1,11 +1,52 @@
-from generichid_keyb import *
+import gobject
 
 import sys
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+import time
+
+
 import os
 
 import pygame
 import pygame.midi
 from pygame.locals import *
+
+
+
+class Demo(object):
+    
+    
+    def __init__(self, hid, hidinput):
+        self.hid = hid
+        self.hidinput = hidinput
+
+        self.hid.connect_to_signal("IncomingConnection",
+                                    lambda : self.connectionMade("IncomingConnection"))
+        self.hid.connect_to_signal("DeviceReleased",
+                                    lambda : self.connectionLost("DeviceReleased"))
+        self.hidinput.connect_to_signal("Reconnected", 
+                                        lambda : self.connectionMade("Reconnected"))
+        self.hidinput.connect_to_signal("Disconnected",
+                                        lambda : self.connectionLost("Disconnected"))
+
+
+    def sendKey(self, a, b, c):
+        print( "sendKey %r %r %r" % (a, b, c))
+        self.hidinput.SendEvent(dbus.Byte(a), dbus.UInt16(b), dbus.Byte(c))
+
+
+    def connectionMade(self, reason):
+        print( "connectionMade %r" % reason)
+        #v = self.hidinput.GetProperties()
+        #print "Properties are %r" % v
+
+
+    def connectionLost(self, reason):
+        print( "connectionLost %r" % reason)
+
+
 
 
 
@@ -30,6 +71,87 @@ def _print_device_info():
                (i, interf, name, opened, in_out))
         
 
+class Keyer(object):
+    """
+    Use keyDown and keyUp as callbacks for triggering the decoder
+    with the relevant chord of keys.
+
+
+    @type  decoder: method
+    @param decoder: called with the chord as a tuple like (0,1,2)
+
+    """
+
+    threshold = 1500
+    decoder = lambda a, b: None
+
+
+    def __init__(self, decoder, threshold=None):
+        if decoder is not None:
+            self.decoder = decoder
+        self.maxtime = 0
+        self.buffer = []
+        self.keyDownbuffer = []
+        if threshold is not None:
+            self.threshold = threshold
+
+
+    def keyUp(self, event, timestamp):
+        if timestamp - self.maxtime > self.threshold:
+            self.buffer = []
+        self.maxtime = timestamp
+        self.buffer.append(event)
+        if event in self.keyDownbuffer:
+            self.keyDownbuffer.remove(event)
+        if self.all_keys_up():
+            self.decoder(tuple(self.buffer))
+            self.buffer = []
+
+
+    def keyDown(self, event, timestamp):
+        self.keyDownbuffer.append(event)
+
+
+    def all_keys_up(self):
+        return self.keyDownbuffer == []
+
+
+class Statefull:
+    def __init__(self):
+        self.keyer = Keyer(self.strokeReceived)
+        self.buf = []
+        self.lastStrokeReceived = time.time()
+
+    def _stroke2string(self, t):
+        return str(t)
+
+    _t = 0.550
+
+    def tick(self):
+        now = time.time()
+        if (now - self.lastStrokeReceived) > self._t:
+            if self.buf:
+                print self.buf
+                self.buf = []
+
+    def strokeReceived(self, p):
+        self.buf.append(self._stroke2string(p))
+        self.lastStrokeReceived = time.time()
+    # states return newstate, data and take data, e
+    
+    def _collectStroke(self, e):
+        if e.type == pygame.midi.MIDIIN:
+            note, status = e.data1, e.status
+            if status == 144: #keydown
+                self.keyer.keyDown(note, time.time())
+            if status == 128:
+                self.keyer.keyUp(note, time.time())
+    
+    def state_init(self, data, e):
+        self._collectStroke(e)
+        return 'init', data
+
+
 def input_main(device_id = None):
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
@@ -52,17 +174,6 @@ def input_main(device_id = None):
 
 
     d = Demo(adapter, in_device)
-    devices = get_devices()
-    for i, dev in enumerate(devices):
-        print("%d %s" % (i, dev))
-
-    print("\n")
-    i = raw_input("which device do you want to try? [0]")
-    if not i:
-        i = 0
-    i = int(i)
-    key_e = devices[i]
-
     #midi stuff
     pygame.init()
     pygame.fastevent.init()
@@ -86,15 +197,22 @@ def input_main(device_id = None):
 
 
     going = True
+    state = 'init'
+    data = None
+    statefull = Statefull()
     while going:
         events = event_get()
+        statefull.tick()
         for e in events:
-            if e.type in [KEYDOWN]:
-                print (e)
+            m = getattr(statefull, 'state_' + state)
+            state, data = m(data, e) 
             if e.type in [QUIT]:
-                going = False
-            if e.type in [pygame.midi.MIDIIN]:
                 print (e)
+            if e.type in [KEYDOWN]:
+                if e.key in [K_ESCAPE]:
+                    going = False
+            #if e.type in [pygame.midi.MIDIIN]:
+            #    print (e)
 
         if mi.poll():
             midi_events = mi.read(10)
